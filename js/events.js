f0093810decea79a9eb16d65d49336cb6c6cea21
Roy Ayala <troysg8203@gmail.com>
2024-08-23 11:35:42 -0400

    fix: modal settings added

--- js/events.js
+++ js/events.js
@@ -3,7 +3,15 @@ window.openInstruction =  function openInstruction() {
     modal.classList.add("modal-active");
 }
 
+window.openSettings =  function openSettings() {
+    console.log("clicked settings");
+    const modal = document.querySelector(".modal-settings");
+    modal.classList.add("modal-active");
+}
+
 window.closeModal =  function closeModal() {
-    const modal = document.querySelector(".modal");
+    const modal = document.querySelector(".modal-active");
     modal.classList.remove("modal-active");
 }
+
+console.log("events.js loaded");
